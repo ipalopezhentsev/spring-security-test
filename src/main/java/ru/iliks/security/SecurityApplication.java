e19a5665f497ac5490a466a1bb635c3cbca586e7
Ilya Palopezhentsev <iliks@web.de>
2024-02-04 15:27:51 +0100

    enable logouts

--- src/main/java/ru/iliks/security/SecurityApplication.java
+++ src/main/java/ru/iliks/security/SecurityApplication.java
@@ -3,25 +3,17 @@ package ru.iliks.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import java.util.List;
 
@@ -97,6 +89,9 @@ public class SecurityApplication {
         //without this, nothing will work even if we pass basic auth credentials, because it will be ignored and
         //users will be switched to anonymousUser, and we don't allow anonymous except actuator
         httpSecurity.httpBasic(withDefaults());
+        //without it, "POST /logout" requires submitting csrf token (which previous methods did not return,
+        //e.g. GET /logout did not return it despite the spring doc saying it should...)
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 }
